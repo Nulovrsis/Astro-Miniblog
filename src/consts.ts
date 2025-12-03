34be96cdfe4299bffba37e145becd449b2e81cc1
Nulovrsis <nulovrsys@gmail.com>
2025-12-03 23:46:09 +0800

    change some data

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_TITLE = "Nulovrsis blog";
 export const SITE_DESCRIPTION =
   "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "sxydisjoint@gmail.com";
